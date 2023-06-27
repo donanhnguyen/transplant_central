4f28cc79040376612a1e17e11ad7d3881ac695a1
thedonwind69@gmail.com <thedonwind69@gmail.com>
2023-06-27 10:00:50 -0500

    render files added

---
+++ bin/render-build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+# clean
+rm -rf public
+# build
+npm install --force
+npm run build
+
+# migrate
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
+# bundle exec rake db:seed
