208c7e67004e3fe50054b45050df70f6d2b9fd89
thedonwind69@gmail.com <thedonwind69@gmail.com>
2023-06-27 21:44:05 -0500

    edited webpack file:

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
   },
-  mode: 'development',
+  mode: 'production',
   module: {
     rules: [
       {
