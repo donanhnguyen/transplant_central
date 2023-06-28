3b505d6995fbabcb13d3c709f4d1eac97b8ec898
thedonwind69@gmail.com <thedonwind69@gmail.com>
2023-06-27 21:33:31 -0500

    edited render file, seeing if it will deploy now

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,11 +6,11 @@ bundle install
 # clean
 rm -rf public
 # build
-npm install --force
+npm install
 npm run build
 
 # migrate
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
-bundle exec rake db:seed
+# bundle exec rake db:seed
